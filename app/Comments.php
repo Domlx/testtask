e9d35f759116d63e94756914ff2d882fe024333c
alexei.dubanovskiy <alexei.dubanovskiy@cyberchisel.com>
2017-03-20 01:17:21 +0200

    seeds and migration

---
+++ app/Comments.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 19.03.2017
+ * Time: 23:56
+ */
---
+++ database/migrations/2017_03_17_232750_add_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id', false, true);
+            $table->string('author');
+            $table->string('text');
+            $table->integer('mark');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
